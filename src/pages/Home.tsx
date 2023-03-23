96423791822a4e0c425e7410c9893a24c7419d98
Brener Rosa <brenersrosa@gmail.com>
2023-03-23 11:30:11 -0300

    :fire: Add and setting react-hook-form

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,30 @@
 import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
 
 export function Home() {
+  const { register, handleSubmit, watch } = useForm()
+
+  function handleCreateNewCicle(data: any) {
+    console.log(data)
+  }
+
+  const task = watch('task')
+  const isSubmitDisable = !task
+
   return (
     <main className="flex-1 flex flex-col items-center justify-center">
-      <form action="" className="flex flex-col items-center gap-14">
+      <form
+        onSubmit={handleSubmit(handleCreateNewCicle)}
+        action=""
+        className="flex flex-col items-center gap-14"
+      >
         <div className="w-full flex items-center justify-center gap-2 text-gray-100 text-lg font-bold flex-wrap">
           <label htmlFor="task">Vou trabalhar em</label>
           <input
             id="task"
             type="text"
             placeholder="Dê um nome para o seu projeto"
+            {...register('task')}
             list="task-suggestion"
             className="bg-transparent h-10 border-0 border-b-2 border-b-solid border-gray-500 font-bold text-lg py-0 px-2 text-gray-100 flex-1 placeholder:text-gray-500 focus:shadow-none focus:border-green-500"
           />
@@ -28,6 +43,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
             className="bg-transparent h-10 w-16 border-0 border-b-2 border-b-solid border-gray-500 font-bold text-lg py-0 px-2 text-gray-100 placeholder:text-gray-500 focus:shadow-none focus:border-green-500"
           />
 
@@ -46,6 +62,7 @@ export function Home() {
 
         <button
           type="submit"
+          disabled={isSubmitDisable}
           className="w-full border-0 p-4 rounded-lg flex items-center justify-center gap-2 font-bold cursor-pointer bg-green-500 text-gray-100 hover:bg-green-700 transition-colors disabled:opacity-70 disabled:cursor-not-allowed disabled:hover:bg-green-500"
         >
           <Play size={24} />
