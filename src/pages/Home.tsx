0db571e6a769c730303b417399dc8e921792b872
Brener Rosa <brenersrosa@gmail.com>
2023-03-28 16:58:12 -0300

    :fire: Reset form

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -30,7 +30,12 @@ export function Home() {
     },
   })
 
-  const { handleSubmit, watch /* reset */ } = newCycleForm
+  const { handleSubmit, watch, reset } = newCycleForm
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    createNewCycle(data)
+    reset()
+  }
 
   const task = watch('task')
   const isSubmitDisable = !task
@@ -38,7 +43,7 @@ export function Home() {
   return (
     <main className="flex-1 flex flex-col items-center justify-center">
       <form
-        onSubmit={handleSubmit(createNewCycle)}
+        onSubmit={handleSubmit(handleCreateNewCycle)}
         action=""
         className="flex flex-col items-center gap-14"
       >
