f7695eac2d8af8a3383fab4674e2128c5c35b5cf
Brener Rosa <brenersrosa@gmail.com>
2023-03-28 17:58:06 -0300

    :fire: History list

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -2,27 +2,6 @@ import clsx from 'clsx'
 import { useContext } from 'react'
 import { CyclesContext } from '../contexts/CyclesContext'
 
-const tasks = [
-  {
-    name: 'Task 1',
-    duration: '20 minutes',
-    start: 'Há 2 meses',
-    status: 'Completed',
-  },
-  {
-    name: 'Task 2',
-    duration: '20 minutes',
-    start: 'Há 2 meses',
-    status: 'In progress',
-  },
-  {
-    name: 'Task 3',
-    duration: '20 minutes',
-    start: 'Há 2 meses',
-    status: 'Interrupted',
-  },
-]
-
 export function History() {
   const { cycles } = useContext(CyclesContext)
 
@@ -30,10 +9,8 @@ export function History() {
     <main className="flex-1 p-14 flex flex-col">
       <h1 className="text-2xl text-gray-100 font-bold">Histórico</h1>
 
-      <pre>{JSON.stringify(cycles, null, 2)}</pre>
-
       <div className="flex-1 overflow-auto mt-8">
-        <table className="w-full border-collapse min-w-[600px]">
+        <table className="w-full border-collapse min-w-[620px]">
           <thead>
             <tr>
               <th className="bg-gray-600 p-4 text-left text-gray-100 text-sm leading-relaxed">
@@ -51,28 +28,33 @@ export function History() {
             </tr>
           </thead>
           <tbody>
-            {tasks.map((task) => (
-              <tr key={task.name}>
+            {cycles.map((cycle) => (
+              <tr key={cycle.id}>
                 <td className="bg-gray-700 border-t-4 border-solid border-gray-800 p-4 text-sm leading-relaxed">
-                  {task.name}
+                  {cycle.task}
                 </td>
                 <td className="bg-gray-700 border-t-4 border-solid border-gray-800 p-4 text-sm leading-relaxed">
-                  {task.duration}
+                  {cycle.minutesAmount} minutos
                 </td>
                 <td className="bg-gray-700 border-t-4 border-solid border-gray-800 p-4 text-sm leading-relaxed">
-                  {task.start}
+                  {cycle.startDate.toISOString()}
                 </td>
                 <td
                   className={clsx(
                     'bg-gray-700 border-t-4 border-solid border-gray-800 p-4 text-sm leading-relaxed flex items-center gap-2 before:w-2 before:h-2 before:rounded-full',
                     {
-                      'before:bg-green-500': task.status === 'Completed',
-                      'before:bg-yellow-500': task.status === 'In progress',
-                      'before:bg-red-500': task.status === 'Interrupted',
+                      'before:bg-green-500': cycle.finishedDate,
+                      'before:bg-red-500': cycle.interruptedDate,
+                      'before:bg-yellow-500':
+                        !cycle.finishedDate && !cycle.interruptedDate,
                     },
                   )}
                 >
-                  {task.status}
+                  {cycle.finishedDate && <span>Concluído</span>}
+                  {cycle.interruptedDate && <span>Interrompido</span>}
+                  {!cycle.finishedDate && !cycle.interruptedDate && (
+                    <span>Em andamento</span>
+                  )}
                 </td>
               </tr>
             ))}
